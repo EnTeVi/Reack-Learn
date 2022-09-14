8befb55758a56178fa3e53f70ff5908ca73de03a
EnTeVi <andriy.lytvynchuk@gmail.com>
2022-09-14 23:28:46 +0300

    less 1 part 2 css

--- src/components/simpson/Simpson.js
+++ src/components/simpson/Simpson.js
@@ -1,7 +1,7 @@
 export default function User(props) {
     let {item: user} = props;
 
-    return (<div>
+    return (<div className='boxUser'>
         <h3>{user.name} {user.surname}</h3>
         <h4>Age: {user.age}</h4>
         <p>{user.info}</p>
