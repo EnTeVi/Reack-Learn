9ef6ac8527e862ad1dac087d98eb3c99de91a217
EnTeVi <andriy.lytvynchuk@gmail.com>
2022-09-14 19:12:47 +0300

    style less 1 finaly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Users from "../src/components/Users/Users";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <h2 className='text-title'>Heroes of the animated series "Rick and Morty"</h2>
+        <Users/>
+      </div>
   );
 }
 
