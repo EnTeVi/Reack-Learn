66157b1f6414293148a28b0cbff9c373b499ba83
EnTeVi <andriy.lytvynchuk@gmail.com>
2022-10-14 19:29:48 +0300

    ts hw

--- interfaces/hw.starlink.interface.ts
+++ interfaces/hw.starlink.interface.ts
@@ -1,50 +1,63 @@
-export interface IStarlink {
+interface IStarlink {
     mission_name: string,
     launch_date_local: string,
     launch_site: {
         site_name_long: string
     },
     links: {
-        article_link: null | string,
+        article_link: string,
         video_link: string
     },
     rocket: {
         rocket_name: string,
         first_stage: {
-            cores: [
-                {
-                    flight: number,
-                    core: {
-                        reuse_count: number,
-                        status: string
-                    }
-                }
-            ]
+            cores: ICores[]
         },
         second_stage: {
-            payloads: [
-                {
-                    payload_type: string,
-                    payload_mass_kg: number,
-                    payload_mass_lbs: number
-                }
-            ]
+            payloads: IPayloads[]
         }
     }
 }
 
+interface ICores {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: unknown | boolean
+    }
+}
 
-const user: {name: string, age: number, gender: string} = {name: 'Max', age: 18, gender: 'man'};
+interface IPayloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
 
-function sum(a:number, b:number) {
+
+
+
+// *****************************
+
+
+
+
+interface IUser {
+    name: string
+    age: number
+    gender: string
+}
+
+const user: IUser = {name: 'Max', age: 18, gender: 'man'};
+
+function sum(a:number, b:number):number {
     return a+b
 }
 
-function showSum(a:number, b:number) {
+function showSum(a:number, b:number):void {
     console.log(a + b);
 }
 
-function incAge(someUser: string | number, inc:number){
+function incAge(someUser: IUser, inc:number):IUser{
     someUser.age += inc
     return someUser
 }
